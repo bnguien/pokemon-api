68fb335c6ec50f95cfa5fe7704ef8f7b581b52ed
bnguien <nguyennguyendangbao8@gmail.com>
2025-10-15 22:38:41 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { renderPokemons, currentPage, filterPokemonByType, searchPokemonByName } from "./ui.js"
+import { renderPokemons, currentPage, filterPokemonByType, searchPokemonByName, createPokemonCardDetail } from "./ui.js"
 
 document.getElementById("prev-btn").onclick = () => {
   if (currentPage > 1) renderPokemons(currentPage - 1)
@@ -17,4 +17,23 @@ document.getElementById("search-input").addEventListener("keypress", (e) => {
 })
 document.getElementById("search-input").addEventListener("blur",(e)=>{
     if(!e.target.value.trim()) filterPokemonByType("all")
+})
+document.getElementById("pokemon-list").addEventListener("click", async (e) => {
+  const card = e.target.closest(".card")
+  if (!card) return
+
+  const name = card.querySelector(".card-title").textContent.trim()
+  const detailCard = await createPokemonCardDetail(name)
+
+  const modal = document.getElementById("pokemon-modal")
+  const modalBody = document.getElementById("modal-body")
+  modalBody.innerHTML = ""
+  modalBody.appendChild(detailCard);
+  modal.style.display = "flex"
+});
+document.getElementById("modal-close").addEventListener("click", () => {
+  document.getElementById("pokemon-modal").style.display = "none"
+})
+document.getElementById("pokemon-modal").addEventListener("click", () => {
+  document.getElementById("pokemon-modal").style.display = "none"
 })
